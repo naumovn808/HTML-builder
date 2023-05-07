8a97244510416fbc36828b709f951946dc6ce5b5
Nikita Naumov <naumovn808@gmail.com>
2023-05-07 22:26:10 +0500

    feat: add solution for task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const fs = require('fs');
+const readline = require('readline');
+
+const { stdin, stdout } = require('process');
+const fileName = 'test.txt';
+const text = 'Введите текст!';
+const goodBye = 'До свидания!';
+
+const filePath = path.resolve(__dirname, fileName);
+const writeableStream = fs.createWriteStream(filePath);
+
+const rl = readline.createInterface({
+  input: stdin,
+  output: stdout,
+});
+
+rl.on('SIGINT', function () {
+  console.log(`\n${goodBye}`);
+  rl.close();
+});
+
+rl.on('line', function (data) {
+  if (data === 'exit') {
+    console.log(`\n${goodBye}`);
+    rl.close();
+  }
+});
+
+console.log(text + '\n');
+stdin.pipe(writeableStream);
